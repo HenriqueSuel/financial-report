954de6cd5abffdb42238d1d308cd3aa37cdc0d91
Lucas Gomes <cprpc@hotmail.com>
2020-05-09 14:35:51 -0300

    Created a signup page and applied a few functions.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { SignupFormModule } from './components/signup/signup-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    SignupComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    SignupFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  user: any = {
+    name: null,
+    surname: null,
+    cpf: null,
+    pass: null
+  }
+
+  nullName: boolean = false
+  nullSurname: boolean = false
+  nullCpf: boolean = false
+  nullPassword: boolean = false
+
+  onSubmit(form) {
+    console.log(form)
+  }
+
+  onClickSubmit() {
+    switch(this.user){
+      case this.user.name === null:
+        this.nullName = true 
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
